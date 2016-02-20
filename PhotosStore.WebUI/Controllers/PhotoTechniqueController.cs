43767939e4a676f951c628d82327683543e3dcc7
krasnyuk <krasnyuk.photo@gmail.com>
2016-02-20 12:06:03 +0200

    Was added paging

--- PhotosStore.WebUI/Controllers/PhotoTechniqueController.cs
+++ PhotosStore.WebUI/Controllers/PhotoTechniqueController.cs
@@ -5,19 +5,36 @@ using System.Web;
 using System.Web.Mvc;
 using PhotosStore.Domain.Abstract;
 using PhotosStore.Domain.Entities;
+using PhotosStore.WebUI.Models;
 
 namespace PhotosStore.WebUI.Controllers
 {
     public class PhotoTechniqueController : Controller
     {
         private IPhotoTechniqueRepository repository;
+        public int pageSize = 4;
+
         public PhotoTechniqueController(IPhotoTechniqueRepository repo)
         {
             repository = repo;
         }
-        public ViewResult List()
+       
+        public ViewResult List(int page = 1)
         {
-            return View(repository.PhotoTechniques);
+            PhotoTechniqueListViewModel model = new PhotoTechniqueListViewModel
+            {
+                PhotoTechniques = repository.PhotoTechniques
+                    .OrderBy(game => game.PhotoTechniqueId)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.PhotoTechniques.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ PhotosStore.WebUI/Models/PhotoTechniqueListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhotosStore.Domain.Entities;
+
+namespace PhotosStore.WebUI.Models
+{
+    public class PhotoTechniqueListViewModel
+    {
+        public IEnumerable<PhotoTechnique> PhotoTechniques { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
