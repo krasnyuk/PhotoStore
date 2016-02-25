2326b3e7c1def889d37ef01f71c1218cfccae2df
krasnyuk <krasnyuk.photo@gmail.com>
2016-02-26 01:35:31 +0200

    was added cart for photo technique

---
+++ PhotosStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhotosStore.Domain.Abstract;
+using PhotosStore.Domain.Entities;
+using PhotosStore.WebUI.Models;
+
+namespace PhotosStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IPhotoTechniqueRepository repository;
+        public CartController(IPhotoTechniqueRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+        public RedirectToRouteResult AddToCart(Cart cart, int photoTechniqueId, string returnUrl)
+        {
+
+            PhotoTechnique photoTechnique = repository.PhotoTechniques.FirstOrDefault(g => g.PhotoTechniqueId == photoTechniqueId);
+
+            if (photoTechnique != null)
+            {
+                cart.AddItem(photoTechnique, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int photoTechniqueId, string returnUrl)
+        {
+            PhotoTechnique photoTechnique = repository.PhotoTechniques.FirstOrDefault(g => g.PhotoTechniqueId == photoTechniqueId);
+
+            if (photoTechnique != null)
+            {
+                cart.RemoveLine(photoTechnique);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
+    }
+}
--- PhotosStore.WebUI/Global.asax.cs
+++ PhotosStore.WebUI/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using PhotosStore.Domain.Entities;
+using PhotosStore.WebUI.Infrastructure.Binders;
 
 namespace PhotosStore.WebUI
 {
@@ -13,6 +15,7 @@ namespace PhotosStore.WebUI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
